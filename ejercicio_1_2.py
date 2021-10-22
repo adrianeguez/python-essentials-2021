db40d11591e65073dca18636cbdea267cb368861
Adrian <eadepto@hotmail.com>
2021-10-22 09:22:53 -0500

    Ejercicios

---
+++ ejercicio_1_2.py
@@ -0,0 +1,6 @@
+iteraciones = 5
+numero_iteracion = 0
+while (numero_iteracion < iteraciones):
+    print("Este tipo de dato en Pyhton es: \n" + str(type(input("Ingrese un valor cualquiera: "))))
+    numero_iteracion = numero_iteracion + 1
+print("¡YA NO SE HARÁN MÁS INTERACCIONES!")
